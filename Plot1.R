11b546f220f5614e700b80de056c7b1d7c717ccd
carinebiessy <biessy@iarc.fr>
2016-11-29 10:14:27 +0100

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,6 @@
 DB <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 DBfinal <- data[DB$Date %in% c("1/2/2007","2/2/2007") ,]
 head(DBfinal)
-hist(DBfinal, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
 electpower<- as.numeric(DBfinal$Global_active_power)
 head(electpower)
