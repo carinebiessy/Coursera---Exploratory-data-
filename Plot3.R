596c5a4d9b8abd947c3011beb37ec0401ae2dcba
carinebiessy <biessy@iarc.fr>
2016-11-29 10:17:17 +0100

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -15,17 +15,10 @@ head(subM2)
 subM3 <- as.numeric(DBfinal$Sub_metering_3)
 head(subM3)
 
-plot(datetime, subM1, type="l", ylab="Energy Submetering")
-
-plot(datetime, subM1, type="l", ylab="Energy Submetering", xlab="")
-
-plot(datetime, subM1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subM2, type="l", col="red")
-
 plot(datetime, subM1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subM2, type="l", col="red")
 lines(datetime, subM3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1, col=c("black", "red", "blue"))
 
 png("Plot3.png", width=480, height=480)
 plot(datetime, subM1, type="l", ylab="Energy Submetering", xlab="")
