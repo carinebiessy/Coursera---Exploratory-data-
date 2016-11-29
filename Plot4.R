63d9fd8e912eac30dd5072cd14a3a08680ec31b8
carinebiessy <biessy@iarc.fr>
2016-11-29 10:18:18 +0100

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -12,10 +12,14 @@ reactpower <- as.numeric(DBfinal$Global_reactive_power)
 par(mfrow = c(2, 2)) 
 plot(datetime,electpower,type="l",xlab="",ylab="Global Active Power")
 
+plot(datetime,volt,type="l",xlab="datetime",ylab="Voltage")
+
 plot(datetime,subM1,type="l",xlab="",ylab="Energy Submetering")
 lines(datetime,subM2,type="l",col="red")
 lines(datetime,subM3,type="l",col="blue")
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,lwd=1,col=c("black","red","blue"))
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,lwd=1,col=c("black", "red", "blue"))
+
+plot(datetime,reactpower,type="l",xlab="datetime",ylab="Global_reactive_power")
 
 
 png("plot4.png", width=480, height=480)
